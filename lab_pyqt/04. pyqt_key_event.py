734f56fe93c310ea2bbfdd9ddd0f8f3f0df7c918
hmandc <hmchang21@gmail.com>
2021-07-12 21:55:50 +0900

    pyqt_key_event

---
+++ lab_pyqt/04. pyqt_key_event.py
@@ -0,0 +1,36 @@
+# 04. pyqt_key_event.py
+# PyQt 키 이벤트
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # 창 크기 고정
+        self.setFixedSize(400, 300)
+        # 창 제목 설정
+        self.setWindowTitle('MyApp')
+
+        label_text = QLabel(self)
+        label_text.setText()
+        label_text.setGeometry(100, 100, 100, 100)
+
+        # 창 띄우기
+        self.show()
+
+    # 키를 누를 때
+    def keyPressEvent(self, event):
+        key = event.key()
+        print(str(key) + ' press')
+
+    # 키를 뗄 때
+    def keyReleaseEvent(self, event):
+        key = event.key()
+        print(str(key) + ' release')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
