938c622ab3bf76eaad4c2087533b87c8b4e6ea1c
gustjd5235 <82814893+gustjd5235@users.noreply.github.com>
2021-09-06 21:43:18 +0900

    도전과제 7하는중

---
+++ lab_mario/06-1.test.py
@@ -0,0 +1,61 @@
+import retro
+import sys
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QPen, QBrush, QColor
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication,\
+    QWidget, QLabel, QPushButton
+import numpy as np
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # 창 크기 고정
+        self.setFixedSize(820, 448)
+        # 창 제목 설정
+        self.setWindowTitle('GA Mario')
+
+        self.press_buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        # 이미지
+        self.label_image = QLabel(self)
+
+        # 게임 환경 생성
+        self.env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+        # 새 게임 시작
+        self.env.reset()
+        # 화면 가져오기
+        self.screen = self.env.get_screen()
+        self.ram = self.env.get_ram()
+
+        qimage = QImage(self.screen, self.screen.shape[1], self.screen.shape[0], QImage.Format_RGB888)
+        pixmap = QPixmap(qimage)
+        pixmap = pixmap.scaled(480, 448, Qt.IgnoreAspectRatio)
+        self.label_image.setPixmap(pixmap)
+
+        env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+        env.reset()
+
+        ram = env.get_ram()
+
+        full_screen_tiles = ram[0x0500:0x069F + 1]
+
+        print(full_screen_tiles.shape)
+        print(full_screen_tiles)
+
+        full_screen_tile_count = full_screen_tiles.shape[0]
+
+        full_screen_page1_tile = full_screen_tiles[:full_screen_tile_count // 2].reshape((13, 16))
+        full_screen_page2_tile = full_screen_tiles[full_screen_tile_count // 2:].reshape((13, 16))
+
+        full_screen_tiles = np.concatenate((full_screen_page1_tile, full_screen_page2_tile), axis=1).astype(np.int)
+
+        print(full_screen_tiles)
+
+    def update_screen(self):
+        # 화면 가져오기
+        self.screen = self.env.get_screen()
+        self.show()
+        screen_qimage = QImage(self.screen, self.screen.shape[1], self.screen.shape[0], QImage.Format_RGB888)
+        pixmap = QPixmap(screen_qimage)
+        pixmap = pixmap.scaled(480, 448, Qt.IgnoreAspectRatio)
+        self.label_image.setPixmap(pixmap)
--- lab_mario/07-1.test.py
+++ lab_mario/07-1.test.py
@@ -1,5 +1,8 @@
 import retro
 import numpy as np
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QTimer
 
 env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
 env.reset()
@@ -19,18 +22,14 @@ full_screen_tiles = np.concatenate((full_screen_page1_tile, full_screen_page2_ti
 
 print(full_screen_tiles)
 
-# 0x071A	Current screen (in level)
-# 현재 화면이 속한 페이지 번호
 current_screen_page = ram[0x071A]
-# 0x071C	ScreenEdge X-Position, loads next screen when player past it?
-# 페이지 속 현재 화면 위치
+
 screen_position = ram[0x071C]
-# 화면 오프셋
+
 screen_offset = (256 * current_screen_page + screen_position) % 512
-# 타일 화면 오프셋
+
 screen_tile_offset = screen_offset // 16
 
-# 현재 화면 추출
 screen_tiles = np.concatenate((full_screen_tiles, full_screen_tiles), axis=1)[:, screen_tile_offset:screen_tile_offset+16]
 
 print(screen_tiles)
